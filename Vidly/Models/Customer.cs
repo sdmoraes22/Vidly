f87eddddd80e1531aa3c7d036fde6b769d068fa0
cmoraes.ti <cmoraes.ti@hotmail.com>
2017-11-11 09:55:30 -0200

    Ajustes de model, views e banco para exibição de data de aniversário.

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -11,8 +11,8 @@ namespace Vidly.Models
         public int Customerid { get; set; }
         [Required]
         [StringLength(255)]
-        public string Name { get; set; }
-        public DateTime BirthDate { get; set; }
+        public string Name { get; set; }        
+        public DateTime? BirthDate { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
